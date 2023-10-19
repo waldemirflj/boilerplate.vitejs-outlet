1dbe9570f7c137d666a06e842cc0a71101b0a1b5
waldemirflj <waldemirflj@gmail.com>
2023-10-19 11:37:47 -0300

    adiciona e configura react-router-dom

---
+++ src/routes/index.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Vite.js</div>,
+  },
+]);
+
+export default router;
